03fc034137c6b320452197307ec8ff56cd3fdf3c
加藤颯真 <kato.soma@meta.co.jp>
2022-03-22 16:35:55 +0900

    usestateへの学び

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Up = ({count, setCount}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => {setCount(count + 1)}}>UP!!</button>
     </div>
-  );
+  )
+}
+
+const Down = ({count, setCount}) => {
+  return (
+    <div>
+      <button onClick={() => {setCount(count - 1)}}>DOWN!!</button>
+    </div>
+  )
+}
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <Up count={count} setCount={setCount}/>
+      <Down count={count} setCount={setCount}/>
+    </div>
+  )
 }
 
 export default App;
